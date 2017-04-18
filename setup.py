c3c2e6665a1ab948a0b5bcb6ed9e2db349c9b39b
jet-black <ovechkin.dm@gmail.com>
2017-04-18 22:41:55 +0300

    fixes

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dimalib',
+      version='1.0',
+      description='My utils',
+      author='jet-black',
+      author_email='ovechkin.dm@gmail.com',
+      url='https://github.com/jet-black/dimalib',
+      packages=['dimalib']
+      )
